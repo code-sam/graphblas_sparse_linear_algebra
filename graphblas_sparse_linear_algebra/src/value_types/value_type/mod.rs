d953870f86dcce63e0b7f115baaf772679da30f8
Sam Dekker <samdekker@gmail.com>
2022-12-07 23:37:40 +0100

    fix: make BuiltInValueType public

--- graphblas_sparse_linear_algebra/src/value_types/value_type/mod.rs
+++ graphblas_sparse_linear_algebra/src/value_types/value_type/mod.rs
@@ -3,7 +3,8 @@ mod custom_value_type;
 mod value_type;
 mod value_type_convertable_to_boolean;
 
-pub(crate) use built_in_value_type::{BuiltInValueType, ConvertScalar, ConvertVector};
+pub(crate) use built_in_value_type::{ConvertScalar, ConvertVector};
 pub(crate) use custom_value_type::RegisteredCustomValueType;
+pub use built_in_value_type::BuiltInValueType;
 pub use value_type::ValueType;
 pub use value_type_convertable_to_boolean::AsBoolean;
