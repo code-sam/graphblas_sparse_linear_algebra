61805d00565caf94a9f096a25b93fd71e9c3fd15
Sam Dekker <samdekker@gmail.com>
2022-12-07 23:56:06 +0100

    fix: do not expose implementation details

--- graphblas_sparse_linear_algebra/src/value_types/value_type/mod.rs
+++ graphblas_sparse_linear_algebra/src/value_types/value_type/mod.rs
@@ -3,8 +3,8 @@ mod custom_value_type;
 mod value_type;
 mod value_type_convertable_to_boolean;
 
-pub(crate) use built_in_value_type::{ConvertScalar, ConvertVector};
+pub(crate) use built_in_value_type::{BuiltInValueType, ConvertScalar, ConvertVector};
 pub(crate) use custom_value_type::RegisteredCustomValueType;
-pub use built_in_value_type::BuiltInValueType;
+// pub use built_in_value_type::BuiltInValueType; // private because it exposes to_graphblas_type()
 pub use value_type::ValueType;
 pub use value_type_convertable_to_boolean::AsBoolean;
