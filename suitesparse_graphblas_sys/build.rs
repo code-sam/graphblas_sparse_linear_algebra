09d8e9eadf2ca0b1c4462686d5da3432784ef7d4
Sam Dekker <samdekker@gmail.com>
2025-10-15 22:53:08 +0200

    fix: use cmake flag use zstd namespace prefix

--- suitesparse_graphblas_sys/build.rs
+++ suitesparse_graphblas_sys/build.rs
@@ -278,8 +278,8 @@ fn build_static_graphblas_implementation(cargo_build_directory: &OsString) {
         .define("CMAKE_INSTALL_LIBDIR", cargo_build_directory.to_owned())
         .define("CMAKE_INSTALL_INCLUDEDIR", cargo_build_directory.to_owned())
         .define("PROJECT_SOURCE_DIR", cargo_build_directory.to_owned())
-        .define("ZSTD_NAMESPACE", "suitesparse_graphblas_zstd_");
-
+        .define("CMAKE_C_FLAGS", "-DZSTD_NAMESPACE=suitesparse_graphblas_zstd_");
+        
     if !cfg!(feature = "build-standard-kernels") {
         build_configuration.define("GRAPHBLAS_COMPACT", "true");
     }
